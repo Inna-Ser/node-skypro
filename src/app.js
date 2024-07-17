4f57b76eab5a3b7910a40b478d11e4ca6612f334
Inna-Ser <serebrakovainna1@gmail.com>
2024-07-17 18:57:24 -0500

    hw2 done

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const { getUsers, getName } = require("./modules/users");
 
 const server = http.createServer((request, response) => {
-  if (request.url === "/?users") {
+  if (request.url === "/users") {
     response.status = 200;
     response.statusMessage = "OK";
     response.header = "Content-Type: application/json";
@@ -15,29 +15,31 @@ const server = http.createServer((request, response) => {
     const name = decodeURIComponent(request.url.split("=")[1]);
     const userName = getName(name);
     if (userName) {
-      response.status = 200;
+      response.statusCode = 200;
       response.statusMessage = "OK";
-      response.header = "Content-Type: text/plain, application/json";
+      response.setHeader = "Content-Type: text/plain, application/json";
       response.end(`Hello, ${userName}`);
     } else {
-      response.status = 400;
+      response.statusCode = 400;
       response.statusMessage = "OK";
-      response.header = "Content-Type: text/plain";
-      response.end("Enter a name");
+      response.setHeader = "Content-Type: text/plain";
+      response.end("Enter a correct name");
     }
     return;
   }
-
-  if (request.url.startsWith("/")) {
-    response.status = 200;
+  if (request.url === "/") {
+    response.statusCode = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
+    response.setHeader = "Content-Type: text/plain";
     response.end("Hello, world!");
+    return;
+  } else {
+    response.statusCode = 500;
+    response.statusMessage = "Bad request";
+    response.setHeader = "Content-Type: text/plain";
+    response.end("Server error");
+    return;
   }
-
-  response.status = 500;
-  response.header = "Content-Type: text/plain";
-  response.end("Server error");
 });
 
 server.listen(3003, () => {
